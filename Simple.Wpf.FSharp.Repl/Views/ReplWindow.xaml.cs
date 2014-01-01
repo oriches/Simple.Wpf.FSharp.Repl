5a5f13f8dbfc96f3e4df979b9131e71ea43dad33
Ollie Riches <ollie.riches@btinternet.com>
2014-01-01 20:58:42 +0000

    Added dynamic theme support

--- Simple.Wpf.FSharp.Repl/Views/ReplWindow.xaml.cs
+++ Simple.Wpf.FSharp.Repl/Views/ReplWindow.xaml.cs
@@ -1,10 +1,6 @@
 ﻿namespace Simple.Wpf.FSharp.Repl.Views
 {
-    using System;
-    using System.Linq;
-    using System.Windows;
     using System.Windows.Controls;
-    using System.Windows.Input;
 
     public partial class ReplWindow : UserControl
     {
--- Test Harnesses/Wpf.TestHarness/MainWindow.xaml.cs
+++ Test Harnesses/Wpf.TestHarness/MainWindow.xaml.cs
@@ -1,12 +1,22 @@
 ﻿namespace Wpf.TestHarness
 {
+    using System;
     using System.Windows;
+    using Simple.Wpf.Themes;
 
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            ThemeControl.ItemsSource = new[]
+            {
+                new Theme("Default theme", null), 
+                new Theme("Blue theme", new Uri("/Wpf.TestHarness;component/Themes/BlueTheme.xaml", UriKind.Relative)), 
+                new Theme("Dark theme", new Uri("/Wpf.TestHarness;component/Themes/DarkTheme.xaml", UriKind.Relative)), 
+                new Theme("Wingding theme", new Uri("/Wpf.TestHarness;component/Themes/WingdingTheme.xaml", UriKind.Relative)), 
+            };
         }
     }
 }
