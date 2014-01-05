4cb7283b34521a34e9d8b6b8284ad2e3ecfe5848
Ollie Riches <ollie.riches@btinternet.com>
2014-01-05 16:57:13 +0000

    Added default style support

--- Simple.Wpf.FSharp.Repl.Themes/Properties/AssemblyInfo.cs
+++ Simple.Wpf.FSharp.Repl.Themes/Properties/AssemblyInfo.cs
@@ -2,8 +2,6 @@
 using System.Runtime.InteropServices;
 using System.Windows;
 
-using Simple.Wpf.Terminal.Themes;
-
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
@@ -51,5 +49,5 @@ using Simple.Wpf.Terminal.Themes;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
